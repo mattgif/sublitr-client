9055847332989f11a992fffbb64c4d4d592c49fd
Matt Gifford <mattgif@gmail.com>
2018-03-22 18:07:44 -0400

    updated test import

--- src/__tests__/commentcard.test.js
+++ src/__tests__/commentcard.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 
-import CommentCard from '../components/commentcard';
+import {CommentCard} from '../components/commentcard';
 
 const defaultComment = {text: '', name: '', date: ''};
 describe('CommentCard', () => {
