83e312eaf562bca6abaa20f5e79a25369ee3c63b
Matt Gifford <mattgif@gmail.com>
2018-03-23 16:04:40 -0400

    Added thunk middleware

--- src/store.js
+++ src/store.js
@@ -1,12 +1,15 @@
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
 import {reducer as formReducer} from 'redux-form';
+import thunk from 'redux-thunk';
+
 import {sublitrReducer} from './reducers/sublitrreducer';
 
 const store = createStore(
     combineReducers({
         form: formReducer,
         sublitr: sublitrReducer
-    })
+    }),
+    applyMiddleware(thunk)
 );
 
 export default store;
