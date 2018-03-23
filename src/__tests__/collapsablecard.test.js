e44290f47fd4b10dd4d5107084c795fba5872241
Matt Gifford <mattgif@gmail.com>
2018-03-23 17:14:44 -0400

    collapse logic tested

--- src/__tests__/collapsablecard.test.js
+++ src/__tests__/collapsablecard.test.js
@@ -6,5 +6,22 @@ import CollapsableCard from '../components/collapsablecard';
 describe('CollapsableCard', () => {
     it('should render without crashing', () => {
         shallow(<CollapsableCard/>);
+    });
+
+    describe('collapse logic', () => {
+        const wrapper = shallow(<CollapsableCard/>);
+        it('should set expanded to false by default', () => {
+            expect(wrapper.state().expanded).toEqual(false);
+        });
+
+        it('should toggle hidden off when card is clicked', () => {
+            wrapper.simulate('click');
+            expect(wrapper.state().expanded).toEqual(true);
+        });
+
+        it('should toggle hidden on when card is clicked a second time', () => {
+            wrapper.simulate('click');
+            expect(wrapper.state().expanded).toEqual(false);
+        });
     })
 });
