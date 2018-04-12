9f2c5df139ccb61ecd8b1f9388bafde5b742f6dd
Matt Gifford <mattgif@gmail.com>
2018-04-12 18:42:54 -0400

    loading animation created

---
+++ src/components/cubic-loading-spinner/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './cubic-loading-spinner.css'
+
+export default function CubicLoadingSpinner() {
+    return (
+        <div className="spinner">
+            <div className="cube1"/>
+            <div className="cube2"/>
+        </div>
+    )
+}
