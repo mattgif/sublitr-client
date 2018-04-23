f0816866500d9dab8c2a2808061f75939425137c
Matt Gifford <mattgif@gmail.com>
2018-04-23 14:20:09 -0400

    style and content update on landing

--- src/components/landing-screen/landing.js
+++ src/components/landing-screen/landing.js
@@ -3,15 +3,16 @@ import {BrowserRouter as Router} from 'react-router-dom';
 import RegistrationForm from "../forms/registration-form/registrationform";
 import Navbar from "../navbar/index";
 import "./landing.css";
-import editorInfoImage from './assets/letter_cat.png';
 import previewImage from './assets/preview_placeholder.png'
+
+const submitterInfoImage = 'https://s3.amazonaws.com/sublitr-images/letter_cat.png';
+const editorInfoImage = 'https://s3.amazonaws.com/sublitr-images/cat_writing.png';
 
 export default function Landing() {
     const scrollTo = sectionId => {
         const scrollToSection = document.getElementById(sectionId);
         scrollToSection.scrollIntoView()
     };
-
     return (
         <Router>
             <div className="App">
@@ -19,43 +20,54 @@ export default function Landing() {
                 <header role="banner" className="landing__hero">
                     <div className="landing__hero__wrapper">
                         <h1 className="landing__hero__title">Submissions simplified</h1>
-                        <h3 className="landing__hero__subtitle">Accept, review, and manage your journal or magazine's submissions</h3>
-                        <button className="landing__hero__scroll__button landing__hero__scroll__signup" onClick={() => scrollTo("signup")}>Join</button>
-                        <button className="landing__hero__scroll__button landing__hero__scroll__more" onClick={() => scrollTo("editor-info")}>Learn more</button>
+                        <h3 className="landing__hero__subtitle">Receive. Review. Decide.</h3>
+                        <button className="landing__hero__scroll__button landing__hero__scroll__signup" onClick={() => scrollTo("signup")}>Sign up</button>
+                        <button className="landing__hero__scroll__button landing__hero__scroll__more" onClick={() => scrollTo("info")}>Learn more</button>
                         <div className="landing__hero__color__image"/>
                     </div>
                 </header>
-                <section id="editor-info" className="editor-info">
-                    <div className="editor-info__wrapper">
-                        <div className="editor-info__body">
-                            <h3>Streamline your review process</h3>
-                            <ul>
-                                <li>View documents on the cloud, with nothing to download</li>
-                                <li>Collaborate with your editorial team with real-time comments</li>
-                                <li>Assign and track statuses throughout your internal review</li>
-                                <li>Automatically notify submitters once a final decision is made</li>
-                            </ul>
+                <section id="info" className="info">
+                    <section className="submitter-info" id="submitter-info">
+                        <div className="submitter-info__wrapper">
+                            <div className="submitter-info__body">
+                                <h3>Receive</h3>
+                                <p>Submitters and editors can manage and track submissions for multiple publications.</p>
+                            </div>
+                            <div className="submitter-info__image">
+                                <img src={submitterInfoImage} alt="cat writing letters"/>
+                            </div>
                         </div>
-                        <div className="editor-info__image">
-                            <img src={editorInfoImage} alt="cat carrying letters"/>
+                    </section>
+                    <section id="editor-info" className="editor-info">
+                        <div className="editor-info__wrapper">
+                            <div className="editor-info__body">
+                                <h3>Review</h3>
+                                <ul>
+                                    <li>Review documents on the cloud</li>
+                                    <li>Add comments and recommendations</li>
+                                    <li>We'll let submitters know when you've made a decision</li>
+                                </ul>
+                            </div>
+                            <div className="editor-info__image">
+                                <img src={editorInfoImage} alt="cat carrying letters"/>
+                            </div>
                         </div>
-                    </div>
-                </section>
-                <section className="app-preview">
-                    <div className="app-preview__wrapper">
-                        <h2>Submission info at a glance</h2>
-                        <p>Access your submission info any time from your Mac, PC, tablet or phone.</p>
-                        <div className="app-preview__image">
-                            <img src={previewImage} alt="sublitr app on phone, pc, and tablet"/>
+                    </section>
+                    <section className="app-preview">
+                        <div className="app-preview__wrapper">
+                            <h2>Submission info at a glance</h2>
+                            <p>Access your submission info any time from your Mac, PC, tablet or phone.</p>
+                            <div className="app-preview__image">
+                                <img src={previewImage} alt="sublitr app on phone, pc, and tablet"/>
+                            </div>
                         </div>
-                    </div>
+                    </section>
                 </section>
                 <section id="signup" className="signup">
                     <div className="signup__wrapper">
                         <h3>Join sublitr</h3>
                         <RegistrationForm/>
                     </div>
-
                 </section>
             </div>
         </Router>
